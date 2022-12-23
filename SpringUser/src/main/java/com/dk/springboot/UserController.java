6defdfe968c565a7da814c199f0b5eb999cf4519
Shivakumarmh <Shivakumar.hiremath005@gmail.com>
2022-12-23 11:23:17 +0530

    Eureka and load balancing

---
+++ SpringUser/src/main/java/com/dk/springboot/UserController.java
@@ -0,0 +1,15 @@
+package com.dk.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@GetMapping("users")
+	public String getUsers()
+	{
+		return "Users fetched";
+	}
+
+}
---
+++ SpringUser/src/test/java/com/dk/springboot/SpringUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
